ff4aa07b7d281541ed9365e4be73828bd7da840c
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 12:09:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def batch_badge_creator(array)
+  array.each do |speaker|
+    puts "Hello, my name is #{speaker}"
+  end 
+end 
+
+def assign_rooms(array)
+  array.each_with_index do |speaker, index|
+    puts "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+end 
