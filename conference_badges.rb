c51cd484a0850002e64554fefe9d6c2331d34d76
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 12:10:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,14 @@ def batch_badge_creator(array)
 end 
 
 def assign_rooms(array)
+  new_array = Array.new 
+  
   array.each_with_index do |speaker, index|
-    puts "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    new_array.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
   end
+  new_array
 end 
 
-def printer
+def printer(array)
+  batch_badge_creator
+  array = ass
